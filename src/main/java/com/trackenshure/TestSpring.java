d898be8fdafefb4c406bfc4f176fa8f21f3dc6f7
Sergey Klunniy <s.klunniy@gmail.com>
2022-01-30 22:53:10 +0200

    add scope prototype

--- src/main/java/com/trackenshure/TestSpring.java
+++ src/main/java/com/trackenshure/TestSpring.java
@@ -1,6 +1,7 @@
 package com.trackenshure;
 
 import com.trackenshure.model.Music;
+import com.trackenshure.model.RockMusic;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -18,8 +19,17 @@ public class TestSpring {
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.playMusic();
 
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
+//        System.out.println(musicPlayer.getName());
+//        System.out.println(musicPlayer.getVolume());
+
+        RockMusic musicBean1 = context.getBean("musicBean1", RockMusic.class);
+        musicBean1.setName("Wind cries Mary !!!!!!!!!!!!!!");
+        MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class);
+
+
+        System.out.println();
+        musicPlayer2.playMusic();
+
 
         context.close();
     }
--- src/main/java/com/trackenshure/model/RockMusic.java
+++ src/main/java/com/trackenshure/model/RockMusic.java
@@ -1,11 +1,20 @@
 package com.trackenshure.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @author Sergey Klunniy
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class RockMusic implements Music {
+    private String name = "Wind cries Mary";
+
     @Override
     public String getSong() {
-        return "RockMusic: Wind cries Mary";
+        return "RockMusic: " + name;
     }
 }

01fcae8d361f8abfb9162850338f293f40e366bb
Sergey Klunniy <s.klunniy@gmail.com>
2022-01-31 22:31:10 +0200

    add init and destroy method

--- src/main/java/com/trackenshure/model/ClassicalMusic.java
+++ src/main/java/com/trackenshure/model/ClassicalMusic.java
@@ -4,6 +4,15 @@ package com.trackenshure.model;
  * @author Sergey Klunniy
  */
 public class ClassicalMusic implements Music {
+
+    public void doMyInit() {
+        System.out.println("Doing my initialization");
+    }
+
+    public void doMyDestroy() {
+        System.out.println("Doing my destruction");
+    }
+
     @Override
     public String getSong() {
         return "ClassicalMusic: Hungarian Rapsody";

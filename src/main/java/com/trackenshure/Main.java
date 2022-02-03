2752ae0fd1bb8545404d294148f35add07fdc775
s.klunniy <s.klunniy@gmail.com>
2022-02-03 04:08:03 -0500

    commit

--- src/main/java/com/trackenshure/Main.java
+++ src/main/java/com/trackenshure/Main.java
@@ -6,8 +6,8 @@ import com.trackenshure.lib.Injector;
 public class Main {
     public static void main(String[] args) {
         //ConsoleHandler consoleHandler = new ConsoleHandler();
-        ConsoleHandler consoleHandler = (ConsoleHandler) Injector.getInstanse(ConsoleHandler.class);
-        System.out.println("Введіть value та risk для вашої ставки:");
-        consoleHandler.handle();
+       // ConsoleHandler consoleHandler = (ConsoleHandler) Injector.getInstanse(ConsoleHandler.class);
+       // System.out.println("Введіть value та risk для вашої ставки:");
+       // consoleHandler.handle();
     }
 }
---
+++ src/main/java/com/trackenshure/model/User.java
@@ -0,0 +1,16 @@
+package com.trackenshure.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    
+    private String name;
+    private String email;
+    
+    // constructors / standard setters / getters
+}
